314a0689923f3dab18ef66c720907a1a8df48a09
pleasantlife <pleasantlife@gmail.com>
2018-01-02 23:06:58 +0900

    20180102 study

--- hello.py
+++ hello.py
@@ -4,5 +4,43 @@ print("Hello World!")
 
 # 변수에 Hello World 넣어 출력하기
 
-a = "Hello World!!"
-print(a)
+hello = "Hello World!!"
+print(hello)
+
+
+# 숫자형 변수
+
+a = 2
+b = 4
+print(a*b)
+print(a**b)
+
+#문자형 변수
+
+multiline = """
+라팔아
+팔렸니
+아니오
+"""
+
+print(multiline)
+
+
+#문자형 변수 연산
+
+plus = '더하기'
+divide = '나누기'
+
+print(plus + divide)
+print(divide * 3)
+
+#문자열 인덱싱
+
+index = '21세기는 정보화시대로서 코딩이 중요하게 여겨집니다.'
+
+print(index[3])
+print(index[-1])
+print(index[-5])
+print(index[:10])
+print(index[7:])
+print(index[0])
